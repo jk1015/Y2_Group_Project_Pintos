971c3f60fae85e6b2e67cf890a070c06dd8b2780
Inusha <inusha@outlook.com>
2017-01-31 15:11:29 +0000

    fixed point arithmetic initial commit

---
+++ src/threads/fixedpoint.h
@@ -0,0 +1,22 @@
+#ifndef FIXED_POINT_H
+#define FIXED_POINT_H
+
+typedef int fixed_point;
+#define BINARY_POINT 14
+#define SHIFT (1 << BINARY_POINT)
+
+#define convert_int_to_fp(x) (x * SHIFT)
+
+#define convert_fp_to_int_round_to_zero(x) (x / SHIFT)
+
+#define convert_fp_to_int_round_to_nearest(x) (x >= 0 ? ((x + (SHIFT / 2)) / SHIFT) : ((x - (SHIFT / 2)) / SHIFT))
+
+/* x is fixed and n is int */
+#define add_f(x, n) (((x) + ((n) * SHIFT)))
+#define sub_f(x, n) (((x) - ((n) * SHIFT)))
+
+/* x and y are fixed */
+#define mult_f(x, y) ((((int64_t) x) * (y) / SHIFT))
+#define divide_f(x, y) ((((int64_t) x) * SHIFT / (y)))
+
+#endif
