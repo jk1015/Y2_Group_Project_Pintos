3b23078409303a4fa21214dac612e447a8d6c3c6
Inusha <inusha@outlook.com>
2017-02-01 09:22:24 +0000

    added signatures to header and added vital import

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -144,6 +144,9 @@ void thread_set_nice (int);
 int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
 
+void thread_update_load_avg (void);
+void thread_update_recent_cpu (struct thread* t, void* aux UNUSED);
+
 bool thread_priority_less_func (
   const struct list_elem *a, const struct list_elem *b, void *aux);
 
