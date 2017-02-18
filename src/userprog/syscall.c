60060a4f3b143054ab99de848def4fdb6a203a9f
James Knight <jk1015@ray08.doc.ic.ac.uk>
2017-02-18 16:56:53 +0000

    Added everything required to run testsuite plus some simple examples

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -8,7 +8,10 @@
 #include "userprog/process.h"
 
 static void syscall_handler (struct intr_frame *);
-static void* deref_user_pointer (const void *uaddr);
+static void* deref_user_pointer (const void *uaddr, uint32_t size);
+
+static int32_t sys_exit(const void* stack);
+static int32_t sys_write(const void* stack);
 static int SYSCALL_AMOUNT;
 
 
@@ -16,14 +19,14 @@ static int SYSCALL_AMOUNT;
 struct syscall
 {
   int number;                       /* System call number. */
-  uint32_t (*function) (void *argv);    /* Function to execute action. */
+  int32_t (*function) (const void *argv);    /* Function to execute action. */
 };
 
 /* Table of supported actions. */
 static const struct syscall syscalls[] =
 {
   {SYS_HALT, NULL},
-  {SYS_EXIT, NULL},
+  {SYS_EXIT, sys_exit},
   {SYS_EXEC, NULL},
   {SYS_WAIT, NULL},
   {SYS_CREATE, NULL},
@@ -31,7 +34,7 @@ static const struct syscall syscalls[] =
   {SYS_OPEN, NULL},
   {SYS_FILESIZE, NULL},
   {SYS_READ, NULL},
-  {SYS_WRITE, NULL},
+  {SYS_WRITE, sys_write},
   {SYS_SEEK, NULL},
   {SYS_TELL, NULL},
   {SYS_CLOSE, NULL}
@@ -48,31 +51,78 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f)
 {
-  void *stack_pointer = deref_user_pointer(f->esp);
+  void *stack_pointer = deref_user_pointer(f->esp, 0);
   int syscall_nr = *((int*) stack_pointer);
-
   for(int i = 0; i < SYSCALL_AMOUNT; i++)
   {
     if(syscalls[i].number == syscall_nr)
     {
-      void** stack = &stack_pointer;
-      f->eax = syscalls[i].function(stack + 1);
+      f->eax = syscalls[i].function(stack_pointer);
+      return;
     }
     ASSERT(i != SYSCALL_AMOUNT - 1);
   }
 }
 
 static void *
-deref_user_pointer (const void *uaddr)
+deref_user_pointer (const void *uaddr, uint32_t size)
 {
   // TODO: Check malloc, lock etc. are freed
   // TODO: Check special cases of strings, buffers, etc.
   if (is_user_vaddr (uaddr))
   {
-    void* ptr = pagedir_get_page (thread_current()->pagedir, uaddr);
-    if (ptr != NULL)
-      return ptr;
+    if (size == 0 || is_user_vaddr((void *) ((char *) uaddr + size)))
+    {
+      void* ptr = pagedir_get_page (thread_current()->pagedir, uaddr);
+      if (ptr != NULL)
+        return ptr;
+    }
   }
-  process_exit();
-  thread_exit();
+  thread_exit ();
+  NOT_REACHED ();
+}
+
+static int32_t
+sys_exit(const void* stack) {
+  //TODO: Wait needs exit_status
+  UNUSED int32_t exit_status = *((int32_t *) stack + 1);
+  thread_exit ();
+  NOT_REACHED ();
+}
+
+static int32_t
+sys_write(const void* stack) {
+  int32_t fd    = *((int32_t *) stack + 1);
+
+  uint32_t size = *((uint32_t *) stack + 3);
+
+  //TODO: WHAT‽
+
+  void *buffer = *((void **) stack + 2);
+
+
+
+  uint32_t ret_size;
+
+  if (fd == 1)
+  {
+    ret_size = size;
+    char *char_buf = (char *) buffer;
+    while (size > 0)
+    {
+      if (size > 200)
+      {
+        putbuf(char_buf, 200);
+      }
+      else
+      {
+        putbuf(char_buf, size);
+        break;
+      }
+      size -= 200;
+      char_buf += 200;
+    }
+    return (int32_t) ret_size;
+  }
+  return 0;
 }
