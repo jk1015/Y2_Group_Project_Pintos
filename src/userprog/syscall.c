98839748e60619735365cdbf3a4da08f6cad0b62
Inusha <inusha@outlook.com>
2017-02-21 13:40:18 +0000

    [syscall] adding sys_exec skeleton

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,13 +6,21 @@
 #include "userprog/pagedir.h"
 #include "threads/vaddr.h"
 #include "userprog/process.h"
+#include "devices/shutdown.h"
+
+#define invalid_memory_error_message "Program attempted to access invalid memory, and has been terminated.\n"
+#define throw_invalid_memory_access() (printf(invalid_memory_error_message));(thread_exit ())
 
 static void syscall_handler (struct intr_frame *);
 static void* deref_user_pointer (const void *uaddr, uint32_t size);
-void retrieve_args(int *call_num, int arg_arr[], int n);
+void retrieve_args (const int *call_num, int arg_arr[], int n);
+int retrieve_ptr (const void *vaddr);
+static void check_user_pointer (const void *uaddr);
 
-static int32_t sys_exit(const void* stack);
-static int32_t sys_write(const void* stack);
+static int32_t sys_halt (const void* stack);
+static int32_t sys_exit (const void* stack);
+static int32_t sys_exec (const void* stack);
+static int32_t sys_write (const void* stack);
 static int SYSCALL_AMOUNT;
 
 
@@ -28,7 +36,7 @@ static const struct syscall syscalls[] =
 {
   {SYS_HALT, sys_halt},
   {SYS_EXIT, sys_exit},
-  {SYS_EXEC, NULL},
+  {SYS_EXEC, sys_exec},
   {SYS_WAIT, NULL},
   {SYS_CREATE, NULL},
   {SYS_REMOVE, NULL},
@@ -65,6 +73,14 @@ syscall_handler (struct intr_frame *f)
   }
 }
 
+static void
+check_user_pointer (const void *uaddr)
+{
+  if (!is_user_vaddr(uaddr) || !pagedir_get_page(thread_current()->pagedir, uaddr)) {
+    throw_invalid_memory_access ();
+  }
+}
+
 static void *
 deref_user_pointer (const void *uaddr, uint32_t size)
 {
@@ -78,23 +94,22 @@ deref_user_pointer (const void *uaddr, uint32_t size)
         return ptr;
     }
   }
-  printf(
-    "Program attempted to access invalid memory, and has been terminated.\n");
-  thread_exit ();
+  throw_invalid_memory_access ();
   NOT_REACHED ();
 }
 
 static int32_t
-sys_halt(const void* stack)
+sys_halt (const void* stack)
 {
   shutdown_power_off();
+  NOT_REACHED ();
 }
 
 static int32_t
-sys_exit(const void* stack)
+sys_exit (const void* stack)
 {
   //TODO: Wait needs exit_status
-  int arg_arr[3];
+  int arg_arr[1];
   retrieve_args (stack, arg_arr, 1);
   // arg_arr[0] is the exit code
   thread_exit ();
@@ -102,7 +117,20 @@ sys_exit(const void* stack)
 }
 
 static int32_t
-sys_write(const void* stack)
+sys_exec (const void* stack)
+{
+  int arg_arr[1];
+  retrieve_args (stack, arg_arr, 1);
+  arg_arr[0] = retrieve_ptr ((void*)arg_arr[0]);                        // translating virtual to kernel address
+  const char* cmd_line = (const char*)arg_arr[0];
+  tid_t tid = process_execute (cmd_line);
+  //TODO: get child process and wait for it to load then return (pid or -1) depending on if it loaded or not
+  return (-1);
+}
+
+
+static int32_t
+sys_write (const void* stack)
 {
   int32_t fd    = *((int32_t *) stack + 1);
 
@@ -142,10 +170,24 @@ sys_write(const void* stack)
 
 // gets arguments from stack
 void
-retrieve_args(int *call_num, int arg_arr[], int n)
+retrieve_args (const int *call_num, int arg_arr[], int n)
 {
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++)
+  {
     int *ptr = (int *)(call_num + 1 + i); 
-    arg_arr[i] = deref_user_pointer(ptr, sizeof(int));
+    arg_arr[i] = (int)deref_user_pointer (ptr, sizeof(int));
   }
+}
+
+int
+retrieve_ptr (const void *vaddr)
+{
+  check_user_pointer(vaddr);
+  int i = (int)pagedir_get_page (thread_current ()->pagedir, vaddr);
+  if(!i)
+  {
+    printf("Invalid Pointer!\n");
+    thread_exit ();
+  }
+  return i;
 }
