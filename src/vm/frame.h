5ef76d9ba6de2ee4f8288bac23efe097147ed57b
Inusha <inusha@outlook.com>
2017-03-09 17:52:25 +0000

    initialising hash table in frame table init

--- src/vm/frame.h
+++ src/vm/frame.h
@@ -2,6 +2,16 @@
 #define FRAME_H
 
 #include <inttypes.h>
+#include <hash.h>
+#include "devices/shutdown.h"
 
+void frame_table_init (void);
+
+struct frame
+{
+  void *page;                   // Pointer to frame owner
+  void *kaddr;                  // Kernel virtual address
+  struct hash_elem hash_elem;   // Frame Hash Table
+};
 
 #endif
